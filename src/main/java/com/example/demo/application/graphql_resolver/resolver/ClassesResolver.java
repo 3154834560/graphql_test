7b8006bbbe65a24b450eff935815a8a20ccad129
王景阳 <3154834560@qq.com>
2023-03-26 16:47:47 +0800

    commit

--- src/main/java/com/example/demo/application/graphql_resolver/resolver/ClassesResolver.java
+++ src/main/java/com/example/demo/application/graphql_resolver/resolver/ClassesResolver.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Component;
 
 /**
  * GraphQLResolver接口是Java程序中实现GraphQL解析的接口，通常用来为GraphQL schema中定义的类型提供自定义的解析方法。
- * java中GraphQLResolver接口是用于定义GraphQL查询和变异的解析器的，因此在query和mutation时都会被调用。
+ * java中GraphQLResolver接口是用于定义GraphQL查询和变异的解析器，因此在query和mutation时都会被调用。
  * <p>
- * 相当于在向前端响应数据前一刻对Classes类型的数据进行拦截，并通过变量名去寻找相同的方法名，执行与变量名相同的方法
+ * 相当于在向前端响应数据前一刻对Classes类型的数据进行拦截，并通过变量名去寻找与之相同的方法名，执行与变量名相同的方法
  * 方法的返回类型必须是与之同名的变量，对应的数据类型，方法必须要用参数，且参数只能为Classes类型
  * 方法不能为private类型
  * <p>
